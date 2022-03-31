14fb05289afdc16c612296e7a58b9ef4a4c79408
Fahmi Dwi Muharom <97666595+Fahmidwimuharom@users.noreply.github.com>
2022-03-31 15:53:46 +0700

    Update Board.cpp

--- 5220600014_Fahmi Dwi Muharom_TicTacToe/Board.cpp
+++ 5220600014_Fahmi Dwi Muharom_TicTacToe/Board.cpp
@@ -1,37 +1,47 @@
 #include "Board.h"
 
-void Board::display()
+void Board3x3::display()
 {
 
 }
 
-bool Board::check_grid_valid()
-{
-	return false;
-}
-
-bool Board::check_grid_full()
+bool Board3x3::check_grid_valid()
 {
 	return false;
 }
 
+bool Board3x3::check_grid_full()
+{
+	return false;
+}
 
-void Board::reset_game()
+
+void Board3x3::reset_game()
 {
 
 }
 
-bool Board::getHorizontal()
+void Board3x3::setValue()
+{
+
+}
+
+char Board3x3::getValue()
 {
 	return 0;
 }
 
-bool Board::getDiagonal()
+char Board3x3::getHorizontal()
 {
 	return 0;
 }
 
-bool Board::getVertical()
+char Board3x3::getDiagonal()
+{
+	return 0;
+}
+
+char Board3x3::getVertical()
 {
 	return 0;
 }
